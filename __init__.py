ab81e9338253c060710e9b23ae29b6ae73650417
JamFfm <Jan.Battermann@t-online.de>
2017-08-20 18:12:28 +0200

    fixed wrong sensor is shown in single mode
    Add hint to the RPLCD Project, 
    throw a notification when LCD address is wrong
    Only singlemode:
    Fixed issue when a wrong sensor is assigned to the kettle when the order of sensors is not the same order of kettles
    Changed beerglasfunktion: The blinking beerglas is only shown when the heater is on

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ from modules import app, cbpi
 from .contextmanagers import cursor, cleared
 from .gpio import CharLCD as GpioCharLCD
 from i2c import CharLCD
-
+LCDVERSION = "3.5.1"
 
 def set_lcd_adress():
 
@@ -21,20 +21,26 @@ def set_lcd_adress():
       cbpi.add_config_parameter("LCD_Adress", "0x27", "string", "Adress of the LCD, CBPi reboot required")
       ref = cbpi.get_config_parameter('LCD_Adress', None)
   if len(ref) != 4:
-      #cbpi.notify("LCD Adress is wrong", "Change LCD Adress in parameters", timeout=None)
-      pass #exeption definieren 
+      cbpi.notify("LCD Adress is not 4 digits", "Change LCD Adress in parameters", type = "warning", timeout=None)
+      pass
   return ref
 LCDadress = int(set_lcd_adress(),16)
 cbpi.app.logger.info("LCDDisplay  - LCD_Adress %s %s" % (set_lcd_adress(),LCDadress))
 
-#adress should be somthing like 0x27, 0x37 etc.
-#comand promt in Raspi: sudo i2cdetect -y 1 or sudo i2cdetect -y 0 to see the adress
-lcd = CharLCD(i2c_expander='PCF8574', address=LCDadress, port=1,
+#adress should be something like 0x27, 0x3f etc.
+#comand promt in Raspi: sudo i2cdetect -y 1 or sudo i2cdetect -y 0 to detect the adress
+#The library and driver are taken from RPLCD Project version 1.0
+#The documentation:   http://rplcd.readthedocs.io/en/stable/ very good and readable
+#Git is here:         https://github.com/dbrgn/RPLCD
+
+try:
+  lcd = CharLCD(i2c_expander='PCF8574', address=LCDadress, port=1,
               cols=20, rows=4, dotsize=8,
               charmap='A00',
               auto_linebreaks=True,
               backlight_enabled=True)
-
+except:
+  cbpi.notify("LCD Adress is wrong", "Change LCD Adress in parameters,\n to detect comand promt in Raspi: sudo i2cdetect -y 1", type = "danger", timeout=None)
 
 def set_parameter_refresh():
 
@@ -52,7 +58,6 @@ def set_parameter_multidisplay():
   multi = cbpi.get_config_parameter('LCD_Multidisplay', None)
   if multi is None:
       cbpi.add_config_parameter("LCD_Multidisplay", "on", "select", "Toggle between all Kettles or show only one Kette constantly, CBPi reboot required", ["on","off"])
-
       multi=cbpi.get_config_parameter('LCD_Multidisplay', None)
   return multi
 
@@ -89,8 +94,9 @@ def get_version_fo(path):
         fo.close()
     finally:
         return version
-version = (get_version_fo(""))
+cbpi_version = (get_version_fo(""))
 
+#building beerglass sign
 bierkrug = (
               0b00000,
 	      0b00000,
@@ -102,7 +108,9 @@ bierkrug = (
 	      0b11100
         )
 lcd.create_char(0, bierkrug)
-global bk #variable for on off of the beerglassymbol (BierKrug) doesnot know better than use semioptimal global var.
+
+#variable for on off of the beerglassymbol (BierKrug) doesnot know better than use semioptimal global var.
+global bk 
 bk = 0
 
 ##Background Task to load the data
@@ -122,11 +130,11 @@ def lcdjob(api):
         if multidisplay == "on":
             lcd.clear()
             for idx, value in cbpi.cache["kettle"].iteritems():
-                current_sensor_value = float(cbpi.get_sensor_value(value.sensor))
+                current_sensor_value = (cbpi.get_sensor_value(value.sensor))
 
                 line1 = (u'%s' % (s.name,))[:20]
 
-                #line2 when steptimer is runnig show remaining time instead of kettlename
+                #line2 when steptimer is runnig show remaining time and kettlename
                 if s.timer_end is not None:
                     time_remaining = time.strftime(u"%H:%M:%S", time.gmtime(s.timer_end - time.time()))
                     line2 = ((u"%s %s" % ((value.name).ljust(12)[:11], time_remaining)).ljust(20)[:20])
@@ -134,7 +142,7 @@ def lcdjob(api):
                     line2 = ((u'%s' % (value.name,))[:20])
 
                 line3 = (u"Targ. Temp:%6.2f%s" % (float(value.target_temp),(u"°C")))[:20]
-                line4 = (u"Curr. Temp:%6.2f%s" % ((current_sensor_value),(u"°C")))[:20]
+                line4 = (u"Curr. Temp:%6.2f%s" % (float(current_sensor_value),(u"°C")))[:20]
 
                 lcd.cursor_pos = (0, 0)
                 lcd.write_string(line1)
@@ -149,30 +157,41 @@ def lcdjob(api):
 
                 pass
         else:
-            #lcd.clear()
             #singlemode
-            current_sensor_value_id1 = float(cbpi.get_sensor_value(id1))
+            #read the current temperature of kettle with id1
+            
+            current_sensor_value_id1 = (cbpi.get_sensor_value(int(cbpi.cache.get("kettle").get(id1).sensor)))
+                                    
+            #get the state of the heater of the current kettle
+            heater_of_kettle = int(cbpi.cache.get("kettle").get(id1).heater)
+            cbpi.app.logger.info("LCDDisplay  - heater id %s" % (heater_of_kettle))
 
+            heater_status = cbpi.cache.get("actors").get(heater_of_kettle).state
+            cbpi.app.logger.info("LCDDisplay  - heater status (0=off, 1=on) %s" % (heater_status))
+            
+            #line1 the stepname
             line1 = (u'%s' % (s.name,)).ljust(20)[:20]
-
-            #line2 when steptimer is runnig show remaining time instead of kettlename
+            
+            #line2 when steptimer is runnig show remaining time and kettlename
             if s.timer_end is not None:
                 time_remaining = time.strftime(u"%H:%M:%S", time.gmtime(s.timer_end - time.time()))
                 line2 = ((u"%s %s" % ((cbpi.cache.get("kettle")[id1].name).ljust(12)[:11],time_remaining)).ljust(20)[:20])
             else:
                 line2 = ((u'%s' % (cbpi.cache.get("kettle")[id1].name)).ljust(20)[:20])
-                
-            line3 = (u"Targ. Temp:%6.2f%s" % (float(cbpi.cache.get("kettle")[id1].target_temp),(u"°C"))).ljust(20)[:20]
-            line4 = (u"Curr. Temp:%6.2f%s" % ((current_sensor_value_id1),(u"°C"))).ljust(20)[:20]
 
+            
+
+            line3 = (u"Targ. Temp:%6.2f%s" % (float(cbpi.cache.get("kettle")[id1].target_temp),(u"°C"))).ljust(20)[:20]
+            line4 = (u"Curr. Temp:%6.2f%s" % (float(current_sensor_value_id1),(u"°C"))).ljust(20)[:20]
+            
 
             lcd.cursor_pos = (0, 0)
             lcd.write_string(line1)
-            lcd.cursor_pos = (0,19)
-            if bk == 0 and s.timer_end is None:        
+            lcd.cursor_pos = (0,19)  
+            if bk == 0 and heater_status != 0:
                 lcd.write_string(u"\x00")
                 global bk
-                bk = 1
+                bk = 1    
             else:
                 lcd.write_string(u" ")
                 global bk
@@ -186,7 +205,7 @@ def lcdjob(api):
 
     else:
         lcd.cursor_pos = (0, 0)
-        lcd.write_string((u"CraftBeerPi %s" % version).ljust(20))
+        lcd.write_string((u"CraftBeerPi %s" % cbpi_version).ljust(20))
         lcd.cursor_pos = (1, 0)
         lcd.write_string((u'%s' % (cbpi.get_config_parameter('brewery_name','No Brewery'))).ljust(20)[:20])
         lcd.cursor_pos =(2, 0)
